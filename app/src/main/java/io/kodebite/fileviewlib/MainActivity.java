2c47177aea8ea33417b1cba794e8d866b261c23b
Mugdha <mugdharahman11@gmail.com>
2023-05-19 13:06:44 +0600

    first blood

--- app/src/main/java/io/kodebite/fileviewlib/MainActivity.java
+++ app/src/main/java/io/kodebite/fileviewlib/MainActivity.java
@@ -3,11 +3,18 @@ package io.kodebite.fileviewlib;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.kodebite.fileviewlib.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
     }
 }
