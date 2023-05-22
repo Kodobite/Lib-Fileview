48dcc299e166b68366377a0262350e8e43f787f8
rahmanmugdha <mugdharahman11@gmail.com>
2023-05-22 16:35:24 +0600

    video view added

---
+++ app/src/main/java/io/kodebite/fileviewlib/VideoViewActivity.java
@@ -0,0 +1,14 @@
+package io.kodebite.fileviewlib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VideoViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_view);
+    }
+}
