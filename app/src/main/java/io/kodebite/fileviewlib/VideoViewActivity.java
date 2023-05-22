f198f83cfe94ef322d4e61daa27e9e6a0edb7b52
rahmanmugdha <mugdharahman11@gmail.com>
2023-05-22 22:49:00 +0600

    video path

--- app/src/main/java/io/kodebite/fileviewlib/VideoViewActivity.java
+++ app/src/main/java/io/kodebite/fileviewlib/VideoViewActivity.java
@@ -1,6 +1,8 @@
 package io.kodebite.fileviewlib;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,11 +12,16 @@ public class VideoViewActivity extends AppCompatActivity {
 
     ActivityVideoViewBinding binding;
 
+    String path = Environment.getExternalStorageDirectory().getPath();
+    String videoPath = path + "/Download/m.mp4";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityVideoViewBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Log.e("VideoViewActivity", "onCreate: " + videoPath);
 
 
     }
